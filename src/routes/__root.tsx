359f069ef531386458c7915cfcfb14b9b7d568df
kjpar0317 <kjpar_kr@yahoo.co.kr>
2025-09-15 12:19:58 +0900

    로그인

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,12 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
+import ErrorPage from "@/components/layout/ErrorPage";
+import NotFoundPage from "@/components/layout/NotFoundPage";
+
 export const Route = createRootRoute({
+	errorComponent: ErrorPage,
+	notFoundComponent: NotFoundPage,
 	component: () => (
 		<>
 			<Outlet />
